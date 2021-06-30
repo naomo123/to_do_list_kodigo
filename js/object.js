eda66998cb6e14f39d7398388e01aa0cd2113c8f
naomo123 <55609446+naomo123@users.noreply.github.com>
2021-06-30 11:52:09 -0600

    first commit

---
+++ js/object.js
@@ -0,0 +1,15 @@
+//{
+//  hora: 1,
+//  turno:AM,
+//  id: 3 es alta , 2 es media , 1 es baja ,
+//  prioridad:"Alta",
+//  tarea: "Jugar",
+//  comentario: "Jugar en el pateo"
+//},
+
+
+
+var toDo =
+[
+
+]
